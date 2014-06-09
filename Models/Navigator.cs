e096ff91ed94442c950a25ad2e4f366713b539f3
AleksanderGalkin <agalkinov@mail.ru>
2014-06-10 00:28:19 +0800

    начало работы над навигатором

---
+++ Models/Navigator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLTest.Models
+{
+
+    public class SearchTerm<T>
+        where T:class,new()
+    {
+        public struct stItemStru { public bool cbItem; public string nmItem; }
+    
+        public IEnumerable<T> model;
+    
+        public string stDescr;
+        public string stField;
+        public List<stItemStru> stList;
+        public  SearchTerm(IEnumerable<T> m,string f,string d)
+        {
+            model = m;
+            stField = f;
+            stDescr = d;
+            var stL = from l in model
+                     group l by l.GetType().GetProperty(stField);
+        }
+        public IEnumerable<T> GetFiltered()
+        {
+            var r=from a in model
+                  join it in stList
+                  on a.GetType().GetProperty(stField).GetValue(a,null).ToString() equals it.nmItem
+                  where it.cbItem
+                  select a;
+            return r;
+        }
+
+    }
+    public class Navigator
+    {
+    }
+}
