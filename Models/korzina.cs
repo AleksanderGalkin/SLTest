8f7f11d82af761c79a851a59575c9b17b465485d
AleksanderGalkin <agalkinov@mail.ru>
2014-07-19 20:38:41 +0800

    корзина

---
+++ Models/korzina.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLTest.Models
+{
+    public class korzina
+    {
+        public struct zakaz {public int drink; public int opt;
+            public zakaz(int d, int o)
+                {
+                    drink = d; opt = o;
+                }
+        }
+        public Dictionary<zakaz, int> diZakaz;
+
+        public korzina(Dictionary<zakaz, int> parZakaz)
+        {
+            diZakaz = parZakaz;
+        }
+
+        
+
+    }
+}
