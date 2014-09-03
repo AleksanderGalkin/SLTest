ea39b9dda39b8200c192aabc480ba5e8f08bf419
AleksanderGalkin <agalkinov@mail.ru>
2014-09-03 19:37:00 +0800

    Подготовлены классы(набросок)

---
+++ Models/OrderDashBoard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLTest.Models
+{
+    public class OrderDashBoard
+    {
+        public int shipToID { get; set; }
+        public int stageID1 { get; set; }
+        public int stageID2 { get; set; }
+        public DateTime stageDT { get; set; }
+        public string user { get; set; }
+
+
+    }
+    public class OrderStage
+    {
+        public int ID { get; set; }
+        public string Descr { get; set; }
+        public string style { get; set; }
+
+    }
+}
